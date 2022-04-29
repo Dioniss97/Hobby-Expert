2b7616d06347d42e1e56cd564a32d027a0f9497f
Dioniss97 <dioniss1997@gmail.com>
2022-04-29 15:56:30 +0200

    dificultades

--- js/app.js
+++ js/app.js
@@ -2,12 +2,10 @@
 let menu = document.getElementById("menu");
 let hamburger = document.getElementById("hamburger");
 
-if(hamburger){
+if (hamburger) {
     hamburger.addEventListener("click", () => {
-
         menu.classList.toggle("active");
         hamburger.classList.toggle("active");
-    
     });
 }
 
@@ -16,13 +14,38 @@ let amount = document.getElementById("amount");
 let plus = document.getElementById("plus");
 let minus = document.getElementById("minus");
 
-
-plus.addEventListener("click", () => {
+if (amount) {
+    plus.addEventListener("click", () => {
         amount.value = (parseInt(amount.value) + 1);
-});
+    });
 
-minus.addEventListener("click", () => {
-    if(amount.value > 1) {
-        amount.value = (parseInt(amount.value) - 1);
-    }
-});
+    minus.addEventListener("click", () => {
+        if (amount.value > 1) {
+            amount.value = (parseInt(amount.value) - 1);
+        }
+    });
+}
+
+// Tabs
+let tabs = document.querySelectorAll("[data-target]");
+let contents = document.querySelectorAll("[data-content]");
+
+if (tabs) {
+
+    tabs.forEach(tab => {
+
+        tab.addEventListener("click", () => {
+            let target = document.querySelector(tab.dataset.target);
+
+            contents.forEach(content => {
+                content.classList.remove("active");
+            });
+            target.classList.add("active");
+
+            tabs.forEach(tab => {
+                tab.classList.remove("active");
+            });
+            tab.classList.add("active");
+        });
+    });
+}
